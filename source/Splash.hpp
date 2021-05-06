edc9e6f1579bc6ef12ee191888b4e3320a734073
Eugene <spring_86@mail.ru>
2021-05-06 19:39:05 +0300

    Добавлен хедер класса анимации брызг

---
+++ source/Splash.hpp
@@ -0,0 +1,18 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <vector>
+
+#include <SFML/Graphics.hpp"
+
+extern sf::VideoMode screen;
+
+class Splashes
+{
+public:
+		
+	
+private:
+	std::vector<sf::Vertex> vertices;
+	float radius;
+	float offset;
+};
