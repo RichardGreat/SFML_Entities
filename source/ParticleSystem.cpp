5e84f03b2d1532cd55537161b8a5a8e61c758730
Eugene3535 <63926730+Eugene3535@users.noreply.github.com>
2021-07-25 11:22:23 +0300

    Update ParticleSystem.cpp

--- source/ParticleSystem.cpp
+++ source/ParticleSystem.cpp
@@ -9,65 +9,66 @@ float radians(int angle)
 	return angle * M_PI / 180.0f;
 }
 
-ParticleSystem::ParticleSystem(sf::Texture& texture, sf::Shader& shader, sf::BlendMode& mode, std::size_t amount) :
-	m_points(sf::Points, amount),
-	m_particles(amount)
+ParticleSystem::ParticleSystem(sf::Texture& texture, sf::Shader& shader, std::size_t amount) :
+    m_points(sf::Points, amount),
+    m_particles(amount)
 {
-	m_states.texture = &texture;
-	m_states.shader = &shader;
-	m_states.blendMode = mode;
+    m_texture = &texture;
+    m_shader = &shader;
 }
 
 ParticleSystem::~ParticleSystem()
 {
-	m_points.clear();
+    m_points.clear();
 }
 
 void ParticleSystem::setEmitter(sf::Vector2f position)
 {
-	m_emitter = position;
+    m_emitter = position;
 }
 
 void ParticleSystem::update(sf::Time delta_time)
 {
-	for (std::size_t i = 0; i < m_particles.size(); ++i)
-	{
-		Particle& particle = m_particles[i];
-		particle.m_lifetime -= delta_time;
+    for (std::size_t i = 0; i < m_particles.size(); ++i)
+    {
+        Particle& particle = m_particles[i];
+        particle.m_lifetime -= delta_time;
 
-		if (particle.m_lifetime <= sf::Time::Zero)
-			resetParticle(i);
+        if (particle.m_lifetime <= sf::Time::Zero)
+            resetParticle(i);
 
-		m_points[i].position += particle.m_velocity * delta_time.asSeconds() / float(screen.width);
+        m_points[i].position += particle.m_velocity * delta_time.asSeconds() / float(screen.width);
 
-		float ratio = particle.m_lifetime / sf::seconds(3.0f);
-		m_points[i].color.a = static_cast<sf::Uint8>(ratio * 255);
-	}
+        float ratio = particle.m_lifetime / sf::seconds(3.0f);
+        m_points[i].color.a = static_cast<sf::Uint8>(ratio * 255);
+    }
 }
 
 sf::Vector2f ParticleSystem::to_ndc(const sf::Vector2f& pos)
 {
-	float scr_half_w = screen.width / 2.0f;
-	float scr_half_h = screen.height / 2.0f;
+    float scr_half_w = screen.width / 2.0f;
+    float scr_half_h = screen.height / 2.0f;
 
-	float x = (pos.x - scr_half_w) / scr_half_w;
-	float y = (pos.y - scr_half_h) / scr_half_h;
+    float x = (pos.x - scr_half_w) / scr_half_w;
+    float y = (pos.y - scr_half_h) / scr_half_h;
 
-	return sf::Vector2f(x, -y);
+    return sf::Vector2f(x, -y);
 }
 
 void ParticleSystem::resetParticle(std::size_t index)
 {
-	float angle = radians(std::rand() % 360);
-	float speed = (std::rand() % 50) + 50.f;
+    float angle = radians(std::rand() % 360);
+    float speed = (std::rand() % 50) + 50.f;
 
-	m_particles[index].m_velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-	m_particles[index].m_lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
+    m_particles[index].m_velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
+    m_particles[index].m_lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
 
-	m_points[index].position = to_ndc(m_emitter);
+    m_points[index].position = to_ndc(m_emitter);
 }
 
 void ParticleSystem::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-	target.draw(m_points, m_states);
+    states.shader = m_shader;
+    states.texture = m_texture;
+    target.draw(m_points, states);
 }
