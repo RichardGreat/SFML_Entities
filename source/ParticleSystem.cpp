dc13d268c4aaa679264a2179d4f70ad572c55430
Eugene3535 <63926730+Eugene3535@users.noreply.github.com>
2021-07-25 17:52:07 +0300

    Удалена функция расчёта NDC

--- source/ParticleSystem.cpp
+++ source/ParticleSystem.cpp
@@ -74,22 +74,11 @@ void ParticleSystem::update(sf::Time delta_time)
         if (particle.m_lifetime <= sf::Time::Zero)
             resetParticle(i);
 
-        m_vertices[i].position += particle.m_velocity * delta_time.asSeconds() / float(screen.width);
+        m_vertices[i].position += particle.m_velocity * delta_time.asSeconds();
 
         float ratio = particle.m_lifetime / m_max_lifetime;
         m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
     }
-}
-
-sf::Vector2f ParticleSystem::to_ndc(const sf::Vector2f& pos)
-{
-    float scr_half_w = screen.width / 2.0f;
-    float scr_half_h = screen.height / 2.0f;
-
-    float x = (pos.x - scr_half_w) / scr_half_w;
-    float y = (pos.y - scr_half_h) / scr_half_h;
-
-    return sf::Vector2f(x, -y);
 }
 
 void ParticleSystem::resetParticle(std::size_t index)
@@ -97,10 +86,10 @@ void ParticleSystem::resetParticle(std::size_t index)
     float dispersion = m_dispersion != 0 ? rand() % static_cast<int>(m_dispersion) - m_dispersion / 2.0f : 0;
     float angle = static_cast<float>((m_direction + dispersion) * M_PI / 180.0f);
 
-    m_particles[index].m_velocity = sf::Vector2f(std::cosf(angle) * m_velocity, std::sinf(angle) * m_velocity);
+    m_particles[index].m_velocity = sf::Vector2f(std::cos(angle) * m_velocity, std::sin(angle) * m_velocity);
     m_particles[index].m_lifetime = sf::milliseconds(std::rand() % m_max_lifetime.asMilliseconds() + 1000);
 
-    m_vertices[index].position = to_ndc(m_emitter);
+    m_vertices[index].position = m_emitter;
 }
 
 void ParticleSystem::draw(sf::RenderTarget& target, sf::RenderStates states) const
