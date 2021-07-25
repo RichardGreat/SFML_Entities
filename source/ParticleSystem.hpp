6a142012498cee539ed4bf5820c4c4464151d41b
Eugene3535 <63926730+Eugene3535@users.noreply.github.com>
2021-07-25 14:41:29 +0300

    Добавлен брифинг

--- source/ParticleSystem.hpp
+++ source/ParticleSystem.hpp
@@ -1,27 +1,51 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
 #include <vector>
+
+#include <SFML/Graphics.hpp>
 
 extern sf::VideoMode screen;
 
 class ParticleSystem :
-    public sf::Drawable
+    public sf::Drawable,
+    private sf::NonCopyable
 {
 public:
-    ParticleSystem(sf::Texture& texture, sf::Shader& shader, std::size_t amount);
+    ParticleSystem(const sf::Texture* texture, sf::Shader* shader, std::size_t amount);
     ~ParticleSystem() = default;
 
+    // Устанавливает точку эмиссии
     void setEmitter(const sf::Vector2f& position);
+
+    // Устанавливает направление эмиссии( параметр: угол в градусах )
     void setDirection(float direction);
+
+    // Устанавливает дисперсию( ширину сектора "распыления") частиц( параметр: угол в градусах )
     void setDispersion(float dispersion);
-    void setVelocity(float power);
+
+    // Устанавливает скорость эмиссии( параметр: скорость в пикселях в секунду )
+    void setVelocity(float velocity);
+
+    // Устанавливает максимальное время жизненного цикла частицы( параметр: значение в объекте sf::Time )
+    void setMaxLifeTime(sf::Time lifetime);
+
+    // Getters
+    const sf::Vector2f& getEmitter()     const;
+    float               getDirection()   const;
+    float               getDispersion()  const;
+    float               getVelocity()    const;
+    const sf::Time&     getMaxLifeTime() const;
 
     void update(sf::Time delta_time);
 
 private:
+    // Перевод экранных координат точки эмиссии в координаты нормализованного устройства (NDC)
     sf::Vector2f to_ndc(const sf::Vector2f& pos);
+
+    // Обновление отдельно взятой частицы по истечении её жизненного цикла
     void resetParticle(std::size_t index);
+
+    // Рендеринг массива частиц
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
     struct Particle
@@ -30,15 +54,14 @@ private:
         sf::Time     m_lifetime;
     };
 
-    sf::Vector2f m_emitter;
+    sf::Vector2f          m_emitter;
+    float                 m_direction;
+    float                 m_dispersion;
+    float                 m_velocity;
 
-    float        m_direction;
-    float        m_dispersion;
-    float        m_velocity;
-
-    const sf::Texture* m_texture;
-    sf::Shader*        m_shader;
-
+    const sf::Texture*    m_texture;
+    sf::Shader*           m_shader;
     sf::VertexArray       m_vertices;
     std::vector<Particle> m_particles;
+    sf::Time              m_max_lifetime;
 };
