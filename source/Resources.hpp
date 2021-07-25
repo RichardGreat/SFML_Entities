5c498d0f10c6c04724945b5f19a76f6d6543a961
Eugene3535 <63926730+Eugene3535@users.noreply.github.com>
2021-07-25 16:38:41 +0300

    Добавлено описание

--- source/Resources.hpp
+++ source/Resources.hpp
@@ -6,3 +6,10 @@
 
 sf::Texture* get_texture(const std::string_view filename) noexcept;
 sf::Font*    get_font(const std::string_view filename)    noexcept;
+
+// Brief:
+//
+// auto texture = get_texture("my_texture");
+// sf::Sprite(*texture);
+// ...
+// Draw it!
