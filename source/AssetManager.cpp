f58941cc173ef4e82c6a47e5989c19e4f2092e41
Eugene3535 <63926730+Eugene3535@users.noreply.github.com>
2021-08-07 20:47:11 +0300

    Замена менеджера ресурсов на шаблонный вариант

---
+++ source/AssetManager.cpp
@@ -0,0 +1,29 @@
+#include "AssetManager.hpp"
+
+#include <filesystem>
+
+template<class Resource>
+void AssetManager<Resource>::load(const std::string_view folder)
+{
+    std::list<std::filesystem::path> file_pathes;
+
+    for (auto& file : std::filesystem::recursive_directory_iterator(folder))
+        file_pathes.emplace_back(file.path());
+
+    for (const auto& path : file_pathes)
+    {
+        Resource resource;
+
+        if (resource.loadFromFile(path.string()))
+            m_resources.emplace(path.stem().string(), resource);       
+    }
+}
+
+template<class Resource>
+Resource* AssetManager<Resource>::get(const std::string& name)
+{
+    if (auto found = m_resources.find(name); found != m_resources.end())
+        return &found->second;
+
+    return nullptr;
+}
