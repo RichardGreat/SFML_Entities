7601905bb50838dab04deb771171b2ffe4d19df7
Eugene3535 <63926730+Eugene3535@users.noreply.github.com>
2021-08-28 10:43:11 +0300

    Расширен класс менеджера анимаций

--- source/AnimationManager.hpp
+++ source/AnimationManager.hpp
@@ -1,41 +1,52 @@
 #pragma once
 
+#include "Animator.hpp"
+
 #include <SFML/Graphics.hpp>
 
-#include <string_view>
+#include <list>
 #include <vector>
 #include <map>
+#include <memory>
 
 // Brief:
 // 
-// auto frames = AnimationManager::get("puppy_walk");
-// float frames_per_sec = 12;
-// 
-// Animator animator(frames, frames_per_sec);
+// auto texture = get_texture("puppy"); // entire texture
+// float frames_per_sec = 12;           // animation speed
+// int duration = 4;                    // amount of frames
+//
+// AnimationManager am();
+// am.create("puppy_walk", texture, duration, frames_per_sec);
+//
+// Animator animator = am.get("puppy_walk");
 // 
 // ..
 // 
-// Play it!
+// Play it! // Note: for more information see the Animator.hpp
 //
 
 class AnimationManager
 {
 public:
-	~AnimationManager() = default;
-
-	// Класс является синглтоном с быстрым и удобным доступом из любой точки приложения
-	static const std::vector<sf::IntRect>* get(const std::string_view id);
-
-private:
 	AnimationManager();
+	~AnimationManager();
 
 	// Создаёт анимацию из кадров, расположенных на текстуре последовательно
 	// Параметры: название текстуры, количество кадров
-	void createAnimation(const std::string_view title, int duration);
-
+	void create(const std::string& id, const sf::Texture* texture, int duration, float rate);
+	
 	// Создаёт анимацию из кадров, расположенных на текстуре таблицей
 	// Параметры: название текстуры, количество кадров в колонке и строке
-	void createAnimation(const std::string_view title, int columns, int rows);
+	void create(const std::string& id, const sf::Texture* texture, int columns, int rows, float rate);
+	
+	// Возвращает копию объекта Animator по значению. Если искомого экземпляра
+	// не было найдено, вернёт объект с нулевыми полями
+	Animator get(const std::string& id) const;
+	
+	// Освобождает все загруженные ресурсы
+	void release();
 
-	std::map<std::string_view, std::vector<sf::IntRect>> m_storage;
+private:
+	std::map<std::string, Animator>                      m_animators; // Экземпляры аниматоров
+	std::list<std::unique_ptr<std::vector<sf::IntRect>>> m_frames;    // Контейнер с уникальными наборами анимаций
 };
