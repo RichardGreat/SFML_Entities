beb8f6dacd05f481c24e0307d0797df052f6325e
Eugene3535 <63926730+Eugene3535@users.noreply.github.com>
2021-08-28 10:44:27 +0300

    Расширен класс менеджера анимаций

--- source/AnimationManager.cpp
+++ source/AnimationManager.cpp
@@ -1,48 +1,58 @@
-#include "Resources.hpp"
 #include "AnimationManager.hpp"
-
-const std::vector<sf::IntRect>* AnimationManager::get(const std::string_view id)
-{
-	static AnimationManager animations;
-
-	if (auto found = animations.m_storage.find(id); found != animations.m_storage.end())
-		return &found->second;
-
-	return nullptr;
-}
 
 AnimationManager::AnimationManager()
 {
-	createAnimation("puppy_walk", 12);   // Будет создана анимация из 12 кадров
-	createAnimation("explosion", 4, 4);  // Будет создана анимация из 16 кадров
 }
 
-void AnimationManager::createAnimation(const std::string_view title, int duration)
+AnimationManager::~AnimationManager()
 {
-	std::vector<sf::IntRect> frames;
-	frames.reserve(duration);
+	release();
+}
 
-	auto tex_size = get_texture(title)->getSize();
+void AnimationManager::create(const std::string& id, const sf::Texture* texture, int duration, float rate)
+{
+	std::unique_ptr<std::vector<sf::IntRect>> frames;
+	frames->reserve(duration);
+
+	auto tex_size = texture->getSize();
 	int frame_width = tex_size.x / duration;
 
 	for (int i = 0; i < duration; i++)
-		frames.emplace_back(i * frame_width, 0, frame_width, tex_size.y);
+		frames->emplace_back(i * frame_width, 0, frame_width, tex_size.y);
 
-	m_storage.emplace(title, std::move(frames));
+	Animator a(frames.get(), rate);
+	m_frames.push_back(std::move(frames));
+	m_animators.emplace(std::move(id), a);
 }
 
-void AnimationManager::createAnimation(const std::string_view title, int rows, int columns)
+void AnimationManager::create(const std::string& id, const sf::Texture* texture, int rows, int columns, float rate)
 {
-	std::vector<sf::IntRect> frames;
-	frames.reserve(columns * rows);
+	std::unique_ptr<std::vector<sf::IntRect>> frames;
+	frames->reserve(columns * rows);
 
-	auto tex_size = get_texture(title)->getSize();
+	auto tex_size = texture->getSize();
 	int frame_width = tex_size.x / columns;
 	int frame_height = tex_size.y / rows;
 
 	for (int y = 0; y < rows; y++)
 		for (int x = 0; x < columns; x++)
-			frames.emplace_back(x * frame_width, y * frame_height, frame_width, frame_height);
-		
-	m_storage.emplace(title, std::move(frames));
+			frames->emplace_back(x * frame_width, y * frame_height, frame_width, frame_height);
+
+	Animator a(frames.get(), rate);
+	m_frames.push_back(std::move(frames));
+	m_animators.emplace(std::move(id), a);
+}
+
+Animator AnimationManager::get(const std::string& id) const
+{
+	if (auto found = m_animators.find(id); found != m_animators.end())
+		return found->second;
+
+	return Animator();
+}
+
+void AnimationManager::release()
+{
+	m_frames.clear();
+	m_animators.clear();
 }
